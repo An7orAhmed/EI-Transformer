3a7d97e7ca463ad8a9f12aeb3c04a08f6a521cb2
AntorOfficial <antor.mee@gmail.com>
2021-08-17 12:05:46 +0600

    bug fixed

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace EI_Transformer_Calculator {
     public partial class Form1 : Form {
